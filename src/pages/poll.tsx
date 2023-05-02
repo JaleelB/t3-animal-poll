6250f8827877554f031ca2a316f8aaf7555e32e1
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-05-01 22:50:53 -0400

    feat: visualizing total poll answers

--- src/pages/poll.tsx
+++ src/pages/poll.tsx
@@ -1,3 +1,4 @@
+import router from 'next/router';
 import { useMultiStepPoll } from '../hooks/useMultiStepForm';
 import Image from 'next/image';
 
@@ -7,23 +8,27 @@ const MultiStepPoll = () => {
   const { step, poll, nextStep, submitAnswer } = useMultiStepPoll(1);
 
   const handleClick = (option: string) => {
-    submitAnswer(option);
-    nextStep();
+    if (step < poll!.maxLength) {
+      submitAnswer(option);
+      nextStep();
+    } else if (step === poll!.maxLength) {
+      router.push('/statistics');
+    }
   };
 
   return (
     <main className='w-screen h-screen bg-neutral-900 flex flex-col justify-center items-center'>
-      <section className='w-[90%] h-[80%] max-w-[680px] flex flex-col'>
-        <h2 className='w-full text-left text-white mb-2'>Step {step < 10 ? `0${step}` : step}/{poll?.maxLength}</h2>
-        <div className='w-full h-full'>
-            <div className="w-full h-full max-h-[250px] lg:max-h-[400px] min-w-[300px]">
+      <section className='w-[90%] max-w-[480px] xl:max-w-[680px] flex flex-col bg-blue-600 p-4 rounded-3xl'>
+        <h2 className='w-fit rounded-full text-left py-2 px-3 text-sm font-medium mb-3 bg-green-300 text-black'>Step {step < 10 ? `0${step}` : step}/{poll?.maxLength}</h2>
+        <div className='h-max'>
+            <div className="">
                 <figure className="w-full h-full rounded-xl bg-neutral-700 relative aspect-video">
-                    <Image 
-                        src={poll?.imageUrl as string}
-                        alt="animal" 
-                        fill
-                        className='object-cover object-center rounded-xl aspect-video'
-                    />
+                  <Image 
+                    src={poll?.imageUrl as string}
+                    alt="animal" 
+                    fill
+                    className='object-cover object-center rounded-xl aspect-video'
+                  />
                 </figure>
             </div>
             <h3 className='text-white text-2xl mt-3'>{poll?.question}</h3>
@@ -34,7 +39,7 @@ const MultiStepPoll = () => {
                             <button 
                                 key={index} 
                                 onClick={()=> handleClick(option)}
-                                className='basis-full md:basis-1/2 bg-white text-gray-900 py-3 md:py-4 rounded-md'
+                                className='basis-full md:basis-1/2 bg-green-300 text-black py-3 md:py-4 rounded-md'
                             >
                                 {option}
                             </button>
--- src/pages/statistics.tsx
+++ src/pages/statistics.tsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { api } from "../utils/api";
 
 const Statistics = () => {
 
+    const questionStatistics = api.questions.getQuestionStatistics.useQuery().data;
+    const totalAnswers = api.questions.getTotalAnswers.useQuery().data;
+    const totalYes = totalAnswers?.totalYes as number;
+    const totalNo = totalAnswers?.totalNo as number;
+    const totalNoWidth = Math.floor(totalNo / (totalYes + totalNo) * 100);
+    const totalYesWidth = Math.floor(totalYes / (totalYes + totalNo) * 100);
+
+
     return (
         <main className='w-screen h-screen bg-neutral-900 flex flex-col justify-center items-center'>
-        <section className='w-full h-full max-w-[800px] flex flex-col py-12 lg:py-18'>
+        <section className='w-[90%] h-full max-w-[800px] flex flex-col py-12 lg:py-18'>
             <h1 className='text-white text-3xl'>Poll Statistics</h1>
             <div className='w-full bg-neutral-800 rounded-lg flex flex-col mt-6 p-8 gap-3'>
                 <h2 className='text-neutral-600 text-lg'>Impressions</h2>
-                <div className="h-12 w-full bg-neutral-700 rounded-lg"></div>
-                <div className="h-12 w-full bg-neutral-700 rounded-lg"></div>
+                <div className="h-12 w-full bg-neutral-700 rounded-lg flex justify-between items-center">
+                    <div 
+                        className={`h-full bg-green-300 rounded-lg flex justify-between items-center px-3`}
+                        style={{transition: 'width 1s ease-in-out', width: `${totalYesWidth}%`}}
+                    >
+                        Yes!
+                    </div>
+                    <span className='text-white pr-3'>{totalAnswers?.totalYes} votes</span>
+                </div>
+                <div className="h-12 w-full bg-neutral-700 rounded-lg flex justify-between items-center">
+                    <div 
+                        className={`h-full bg-blue-600 rounded-lg flex justify-between items-center px-3`}
+                        style={{transition: 'width 1s ease-in-out', width: `${totalNoWidth}%`}}
+                    >
+                        No!
+                    </div>
+                    <span className='text-white pr-3'>{totalAnswers?.totalNo} votes</span>
+                </div>
             </div>
         </section>
         </main>

a81b8294137ebf6420f2a65d32bb0eb303e76f60
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-05-02 13:36:30 -0400

    feat: implemented statistics per question

--- src/pages/poll.tsx
+++ src/pages/poll.tsx
@@ -1,5 +1,5 @@
 import router from 'next/router';
-import { useMultiStepPoll } from '../hooks/useMultiStepForm';
+import { useMultiStepPoll } from '../hooks/useMultiStepPoll';
 import Image from 'next/image';
 
 
@@ -17,12 +17,13 @@ const MultiStepPoll = () => {
   };
 
   return (
-    <main className='w-screen h-screen bg-neutral-900 flex flex-col justify-center items-center'>
-      <section className='w-[90%] max-w-[480px] xl:max-w-[680px] flex flex-col bg-blue-600 p-4 rounded-3xl'>
-        <h2 className='w-fit rounded-full text-left py-2 px-3 text-sm font-medium mb-3 bg-green-300 text-black'>Step {step < 10 ? `0${step}` : step}/{poll?.maxLength}</h2>
+    <main className='w-screen h-screen bg-zinc-50 flex flex-col justify-center items-center'>
+      <section className='w-[90%] max-w-[480px] xl:max-w-[680px] flex flex-col p-4 border bg-white rounded-2xl'>
+        <h2 className='w-fit rounded-full text-left text-sm font-medium mb-3 text-gray-600'>Question {step < 10 ? `0${step}` : step}/{poll?.maxLength}</h2>
         <div className='h-max'>
+            <h3 className='text-gray-900 text-2xl '>{poll?.question}</h3>
             <div className="">
-                <figure className="w-full h-full rounded-xl bg-neutral-700 relative aspect-video">
+                <figure className="w-full h-full rounded-xl bg-violet-200 relative aspect-video mt-3">
                   <Image 
                     src={poll?.imageUrl as string}
                     alt="animal" 
@@ -31,7 +32,7 @@ const MultiStepPoll = () => {
                   />
                 </figure>
             </div>
-            <h3 className='text-white text-2xl mt-3'>{poll?.question}</h3>
+            
             <div className='flex flex-col md:flex-row gap-2 mt-3'>
                 {
                     poll?.options.map((option: string, index: number)=>{
@@ -39,7 +40,7 @@ const MultiStepPoll = () => {
                             <button 
                                 key={index} 
                                 onClick={()=> handleClick(option)}
-                                className='basis-full md:basis-1/2 bg-green-300 text-black py-3 md:py-4 rounded-md'
+                                className={`basis-full md:basis-1/2 ${index === 0 ? 'bg-neutral-900 text-gray-200' : 'border-2 border-black text-gray-900'} py-3 md:py-4 rounded-md`}
                             >
                                 {option}
                             </button>
--- src/pages/statistics.tsx
+++ src/pages/statistics.tsx
@@ -12,31 +12,83 @@ const Statistics = () => {
 
 
     return (
-        <main className='w-screen h-screen bg-neutral-900 flex flex-col justify-center items-center'>
-        <section className='w-[90%] h-full max-w-[800px] flex flex-col py-12 lg:py-18'>
-            <h1 className='text-white text-3xl'>Poll Statistics</h1>
-            <div className='w-full bg-neutral-800 rounded-lg flex flex-col mt-6 p-8 gap-3'>
-                <h2 className='text-neutral-600 text-lg'>Impressions</h2>
-                <div className="h-12 w-full bg-neutral-700 rounded-lg flex justify-between items-center">
-                    <div 
-                        className={`h-full bg-green-300 rounded-lg flex justify-between items-center px-3`}
-                        style={{transition: 'width 1s ease-in-out', width: `${totalYesWidth}%`}}
-                    >
-                        Yes!
+        <main className='w-screen min-h-screen bg-zinc-50 flex flex-col items-center'>
+            <section className='w-[90%] max-w-[800px] flex flex-col py-16 lg:py-24'>
+                <h1 className='text-gray-900 text-4xl'>Poll Statistics</h1>
+                <p className='text-gray-500 text-sm'>See the responses of others who took this poll</p>
+                <div className='w-full bg-white rounded-lg flex flex-col mt-6 p-8 gap-3 border'>
+                    <h2 className='text-neutral-600 mb-3'>Impressions</h2>
+                    
+                    <div className='flex flex-col-reverse gap-1 w-full'>
+                        <div
+                            className={`h-3 bg-green-300 rounded-lg flex justify-between items-center px-3 font-medium`}
+                            style={{transition: 'width 1s ease-in-out', width: `${totalYesWidth}%`}}
+                        >
+                        </div>
+                        <div className='w-full flex justify-between items-center'>
+                            <span className='font-medium'>Yes!</span>
+                            <span className='text-gray-900 text-right font-medium flex'>{`${totalYesWidth}%`}</span>
+                        </div>
+                        
                     </div>
-                    <span className='text-white pr-3'>{totalAnswers?.totalYes} votes</span>
-                </div>
-                <div className="h-12 w-full bg-neutral-700 rounded-lg flex justify-between items-center">
-                    <div 
-                        className={`h-full bg-blue-600 rounded-lg flex justify-between items-center px-3`}
-                        style={{transition: 'width 1s ease-in-out', width: `${totalNoWidth}%`}}
-                    >
-                        No!
+                    <div className='flex flex-col-reverse gap-1 w-full'>
+                        <div
+                            className={`h-3 bg-blue-600 rounded-lg flex justify-between items-center px-3 font-medium`}
+                            style={{transition: 'width 1s ease-in-out', width: `${totalNoWidth}%`}}
+                        >
+                        </div>
+                        <div className='w-full flex justify-between items-center'>
+                            <span className='font-medium'>No!</span>
+                            <span className='text-gray-900 text-right font-medium flex'>{`${totalNoWidth}%`}</span>
+                        </div>
+                        
                     </div>
-                    <span className='text-white pr-3'>{totalAnswers?.totalNo} votes</span>
+                    <h2 className='text-neutral-600 text-right mt-3'>{`${(totalAnswers?.totalNo as number) + (totalAnswers?.totalYes as number)} votes`}</h2>
                 </div>
-            </div>
-        </section>
+            </section>
+
+            <section className='w-[90%] max-w-[800px] py-12 lg:py-18'>
+                <h2 className='text-gray-600 text-2xl text-left mb-3'>Votes per question</h2>
+                <div className='grid grid-cols-1 gap-3'>
+                {
+                    questionStatistics?.map((question: any, index: number)=>{
+                        return (
+                            <div key={index} className='w-full bg-white rounded-lg flex flex-col gap-3 p-8 border'>
+                                <h3 className='text-gray-900 text-lg mb-2'>{question.question}</h3>
+                                <div className="w-full flex justify-between items-center">
+                                    <div className='flex flex-col-reverse gap-1 w-full'>
+                                        <div
+                                            className={`h-3 bg-green-300 rounded-lg flex justify-between items-center px-3 font-medium`}
+                                            style={{transition: 'width 1s ease-in-out', width: `${Math.floor(question.statistics.Yes / (question.statistics.Yes + question.statistics.No) * 100)}%`}}
+                                        >
+                                        </div>
+                                        <div className='w-full flex justify-between items-center'>
+                                            <span className='font-medium'>Yes!</span>
+                                            <span className='text-gray-900 text-right font-medium flex'>{`${question.statistics.Yes} votes`}</span>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                                 <div className="w-full flex justify-between items-center">
+                                    <div className='flex flex-col-reverse gap-1 w-full'>
+                                        <div
+                                            className={`h-3 bg-blue-600 rounded-lg flex justify-between items-center px-3 font-medium`}
+                                            style={{transition: 'width 1s ease-in-out', width: `${Math.floor(question.statistics.No / (question.statistics.Yes + question.statistics.No) * 100)}%`}}
+                                        >
+                                        </div>
+                                        <div className='w-full flex justify-between items-center'>
+                                            <span className='font-medium'>No!</span>
+                                            <span className='text-gray-900 text-right font-medium flex'>{`${question.statistics.No} votes`}</span>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+                </div>
+            </section>
         </main>
     )
 }
